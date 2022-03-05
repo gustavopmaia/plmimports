cee7802d28b731730fadcbe528c6bdb3b14e6e64
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-03-05 14:35:53 -0300

    feat: Delete user

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,6 +1,8 @@
-import * as Yup from 'yup'
-import * as argon2 from 'argon2'
-import User from '../models/User'
+import * as Yup from 'yup';
+import * as argon2 from 'argon2';
+import mongoose from 'mongoose';
+
+import User from '../models/User';
 
 class UserController {
   async store(req, res) {
@@ -11,7 +13,7 @@ class UserController {
         error: true,
         code: 102,
         message: 'Error: Email ja existe3',
-      })
+      });
     }
 
     // Yup validacao
@@ -20,14 +22,14 @@ class UserController {
       name: Yup.string().required(),
       email: Yup.string().email().required(),
       password: Yup.string().required().min(6),
-    })
+    });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({
         error: true,
         code: 103,
         message: 'Error: Dados invalidos',
-      })
+      });
     }
 
     // Criptografar senha
@@ -35,19 +37,56 @@ class UserController {
     dados.password = await argon2.hash(dados.password);
 
     // Criar usuario
-    const user = await User.create(dados, (err) => {
-      if (err)
+    const user = await User.create(dados)
+      .then(() => {
+        return res.status(200).json({
+          error: false,
+          message: 'Usuário cadastrado com sucesso',
+        });
+      })
+      .catch((err) => {
         return res.status(400).json({
           error: true,
           code: 101,
           message: 'Error: Usuário não cadastrado' + err,
-        })
-      return res.status(200).json({
-        error: false,
-        message: 'Usuário cadastrado com sucesso',
+        });
+      });
+  }
+
+  async delete(req, res) {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({
+        error: true,
+        code: 121,
+        message: 'Erro: Usuário não encontrado',
+      });
+    }
+
+    const usuarioExiste = await User.findOne({ _id: req.params.id });
+
+    if (!usuarioExiste) {
+      return res.status(400).json({
+        error: true,
+        code: 121,
+        message: 'Error: Usuário não encontrado',
+      });
+    }
+
+    const user = await User.deleteOne({ _id: req.params.id })
+      .then(() => {
+        return res.json({
+          error: false,
+          message: 'Usuário apagado com sucesso!',
+        });
       })
-    })
+      .catch((err) => {
+        return res.status(400).json({
+          error: true,
+          code: 122,
+          message: 'Error: Usuário não foi apagado' + err,
+        });
+      });
   }
 }
 
-export default new UserController()
+export default new UserController();
