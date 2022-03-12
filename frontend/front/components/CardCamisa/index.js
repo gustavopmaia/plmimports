977c6d87820ec5c7d3abc82dcdbaf47bb82fc215
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-03-12 19:51:07 -0300

    feat: Image modal

--- frontend/front/components/CardCamisa/index.js
+++ frontend/front/components/CardCamisa/index.js
@@ -1,28 +1,74 @@
 import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 import Image from 'next/image';
 import Link from 'next/link';
 
-function CardCamisa(props) {
-  return (
-    <>
+class CardCamisa extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  }
+
+  render() {
+    return (
       <div className='card'>
         <div className='img-wrapper'>
-          <Image src={props.src} alt='Camisa' className='card-img-top hover-zoom' layout='responsive' width={280} height={280} />
+          <a onClick={this.toggle}>
+            <Image
+              src={this.props.src}
+              alt='Camisa'
+              className='card-img-top hover-zoom card-img'
+              layout='responsive'
+              width={280}
+              height={280}
+            />
+          </a>
+          <Modal
+            isOpen={this.state.modal}
+            toggle={this.toggle}
+          >
+            <ModalHeader toggle={this.toggle}>
+              {this.props.children}
+            </ModalHeader>
+            <ModalBody>
+              <Image
+                src={this.props.src}
+                alt='Camisa'
+                layout='responsive'
+                className="card-img-top hover-zoom"
+                width={280}
+                height={280}
+              />
+            </ModalBody>
+          </Modal>
         </div>
         <div className='card-body'>
-          <h5 className='card-title'>{props.children}</h5>
+          <h5 className='card-title'>{this.props.children}</h5>
         </div>
         <div className='card-footer'>
-          <Link target="_blank" href='https://api.whatsapp.com/send/?phone=5511981476124&text&app_absent=0'>
-            <a target="_blank" className='btn btn-primary'>
+          <Link
+            target='_blank'
+            href='https://api.whatsapp.com/send/?phone=5511981476124&text&app_absent=0'
+          >
+            <a target='_blank' className='btn btn-primary'>
               Compre agora
             </a>
           </Link>
         </div>
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default CardCamisa;
