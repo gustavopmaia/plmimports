607b3c6bec6b5b3fb74c18fc3e8fabd3c9465063
Gustavo Maia <gustavopmaia@yahoo.com.br>
2022-03-26 21:47:10 -0300

    fix: Sizes

--- front/components/Sizes/index.js
+++ front/components/Sizes/index.js
@@ -8,10 +8,10 @@ function Sizes(props) {
         <h2 className='text-center p-3'>Tamanho Masculino</h2>
         <Row className='justify-content-center'>
           <Col className='bg-light border' xs='3'></Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             Largura
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             Comprimento
           </Col>
         </Row>
@@ -20,10 +20,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho P
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             48cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             68cm
           </Col>
         </Row>
@@ -32,10 +32,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho M
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             50cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             70cm
           </Col>
         </Row>
@@ -44,10 +44,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho G
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             52cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             73cm
           </Col>
         </Row>
@@ -56,10 +56,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho GG
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             56cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             76cm
           </Col>
         </Row>
@@ -68,10 +68,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho XXL
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             58cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             79cm
           </Col>
         </Row>
@@ -80,10 +80,10 @@ function Sizes(props) {
 
         <Row className='justify-content-center'>
           <Col className='bg-light border' xs='3'></Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             Largura
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             Comprimento
           </Col>
         </Row>
@@ -92,10 +92,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho P
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             40cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             61cm
           </Col>
         </Row>
@@ -104,10 +104,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho M
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             44cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             66cm
           </Col>
         </Row>
@@ -116,10 +116,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho G
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             47cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             69cm
           </Col>
         </Row>
@@ -128,10 +128,10 @@ function Sizes(props) {
           <Col className='bg-light border' xs='3'>
             Tamanho GG
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             50cm
           </Col>
-          <Col className='bg-light border' xs='3'>
+          <Col className='bg-light border' xs='4'>
             71cm
           </Col>
         </Row>
